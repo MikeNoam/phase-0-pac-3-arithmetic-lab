8e201046987f0362ca76fd7f942c26d0ccd72496
MichaelHarris <mike.n.harris1@gmail.com>
2023-01-18 11:10:44 -0500

    Success git commit -m Success cd . :

---
+++ index.js
@@ -0,0 +1,47 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add() {
+    return "something";
+}
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b; 
+}
+function multiply (a, b) {
+    return a * b; 
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+function makeInt(string) {
+   parseInt("string", 10);
+   return(parseInt(string, 10));
+}
+function preserveDecimal(string) {
+  parseFloat("string", 10);
+    return(parseFloat(string, 10));
+}
+    
